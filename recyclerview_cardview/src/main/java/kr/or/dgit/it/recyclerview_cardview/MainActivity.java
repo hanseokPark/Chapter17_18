e6eec6f69c85114a3bd41aabb7acb046645132f2
hanseok Park <es12850@naver.com>
2018-05-23 19:01:48 +0900

    2018-05-23 NavigationView

--- recyclerview_cardview/src/main/java/kr/or/dgit/it/recyclerview_cardview/MainActivity.java
+++ recyclerview_cardview/src/main/java/kr/or/dgit/it/recyclerview_cardview/MainActivity.java
@@ -7,13 +7,10 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,13 +21,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-
-public class MainActivity extends AppCompatActivity {
-
-
-    private ActionBarDrawerToggle toggle;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,56 +30,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        final DrawerLayout drawerLayout = findViewById(R.id.main_drawer);
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        DrawerLayout drawerLayout = findViewById(R.id.main_drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,  R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.main_drawer_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                int id = item.getItemId();
-                if(id==R.id.menu_drawer_home){
-                    showToast("NavigationDrawer...home..");
-                }else if(id==R.id.menu_drawer_message){
-                    showToast("NavigationDrawer...message..");
-                }else if(id==R.id.menu_drawer_add){
-                    showToast("NavigationDrawer...add..");
-                }
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return false;
-
-            }
-
-        });
-
-
+        navigationView.setNavigationItemSelectedListener(this);
 
         //원본
-        Item[] items = {
+        Item[] items ={
                 new Item("Chapter One", "Item one details", R.drawable.android_image_1),
-                new Item("Chapter Two", "Item Two details", R.drawable.android_image_2),
-                new Item("Chapter Three", "Item Three details", R.drawable.android_image_3),
-                new Item("Chapter Four", "Item Four details", R.drawable.android_image_4),
-                new Item("Chapter Five", "Item Five details", R.drawable.android_image_5),
-                new Item("Chapter Six", "Item Six details", R.drawable.android_image_6),
-                new Item("Chapter Seven", "Item Seven details", R.drawable.android_image_7),
-                new Item("Chapter Eight", "Item Eight details", R.drawable.android_image_8),
+                new Item("Chapter Two", "Item two details", R.drawable.android_image_2),
+                new Item("Chapter Three", "Item three details", R.drawable.android_image_3),
+                new Item("Chapter Four", "Item four details", R.drawable.android_image_4),
+                new Item("Chapter Five", "Item five details", R.drawable.android_image_5),
+                new Item("Chapter Six", "Item six details", R.drawable.android_image_6),
+                new Item("Chapter Seven", "Item seven details", R.drawable.android_image_7),
+                new Item("Chapter Eight", "Item eight details", R.drawable.android_image_8),
         };
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-
-        /*RecyclerView.LayoutManager layoutManager1 = new GridLayoutManager(this, 2);
-        RecyclerView.LayoutManager layoutManager2 = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);*/
-
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(new RecyclerAdapter(items));
 
@@ -100,19 +63,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
     }
 
+    private void showToast(String message){
+        Toast toast=Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(toggle.onOptionsItemSelected(item)){
-            return false;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        return super.onOptionsItemSelected(item);
     }
-    private void showToast(String message){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.menu_drawer_home){
+            showToast("NavigationDrawer... home...");
+        }else if(id==R.id.menu_drawer_message){
+            showToast("NavigationDrawer... message...");
+        }else if(id==R.id.menu_drawer_add){
+            showToast("NavigationDrawer... add...");
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawer);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private class RecyclerAdapter extends RecyclerView.Adapter {
@@ -127,16 +107,13 @@ public class MainActivity extends AppCompatActivity {
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_layout, parent, false);
             ViewHolder viewHolder = new ViewHolder(view);
-
             return viewHolder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
             ViewHolder viewHolder = (ViewHolder) holder;
-
             Item item = items[position];
-
             viewHolder.itemTitle.setText(item.title);
             viewHolder.itemDetail.setText(item.detail);
             viewHolder.itemImage.setImageResource(item.img);
@@ -146,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return items.length;
         }
-
 
         class ViewHolder extends RecyclerView.ViewHolder{
             ImageView itemImage;
@@ -163,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         int position = getAdapterPosition();
-                        Snackbar.make(v, "Click selected on Item" + position, Snackbar.LENGTH_LONG)
+                        Snackbar.make(v, "Click selected on Item " + position, Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).show();
                     }
                 });
